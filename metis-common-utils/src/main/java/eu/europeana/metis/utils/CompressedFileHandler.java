c1b42606db7290d5f952c38ee67fbf878be79d12
Simon Tzanakis <simon.tzanakis@europeana.eu>
2025-10-16 13:53:42 +0200

    MET-6841 Remove unused methods

--- metis-common-utils/src/main/java/eu/europeana/metis/utils/CompressedFileHandler.java
+++ metis-common-utils/src/main/java/eu/europeana/metis/utils/CompressedFileHandler.java
@@ -1,15 +1,10 @@
 package eu.europeana.metis.utils;
 
-import static eu.europeana.metis.utils.TempFileUtils.createSecureTempFile;
-
 import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.invoke.MethodHandles;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -17,20 +12,16 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
 import org.apache.commons.compress.archivers.ArchiveStreamFactory;
 import org.apache.commons.compress.compressors.CompressorException;
 import org.apache.commons.compress.compressors.CompressorStreamFactory;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -255,67 +246,5 @@ public class CompressedFileHandler {
       throw new IOException("Entry is outside of the target dir: " + entryName);
     }
     return normalizePath;
-  }
-
-  /**
-   * This method extracts all files from a ZIP file and returns them as byte arrays. This method only considers files in the main
-   * directory. This method creates (and then removes) a temporary file.
-   *
-   * @param providedZipFile Input stream containing the zip file. This method is not responsible for closing the stream.
-   * @return A list of records.
-   * @throws IOException In case of problems with the temporary file or with reading the zip file.
-   */
-  public List<ByteArrayInputStream> getContentFromZipFile(InputStream providedZipFile)
-      throws IOException {
-    try (final ZipFile zipFile = createInMemoryZipFileObject(providedZipFile)) {
-      final List<InputStream> streams = getContentFromZipFile(zipFile);
-      final List<ByteArrayInputStream> result = new ArrayList<>(streams.size());
-      for (InputStream stream : streams) {
-        result.add(new ByteArrayInputStream(IOUtils.toByteArray(stream)));
-      }
-      return result;
-    }
-  }
-
-  private ZipFile createInMemoryZipFileObject(InputStream content) throws IOException {
-    final File tempFile = createSecureTempFile(content.getClass().getSimpleName(), ".zip").toFile();
-    FileUtils.copyInputStreamToFile(content, tempFile);
-    LOGGER.info("Temp file: {} created.", tempFile);
-    return new ZipFile(tempFile, ZipFile.OPEN_READ | ZipFile.OPEN_DELETE);
-  }
-
-
-  /**
-   * Retrieves records from a given ZIP file. Each record is extracted from the files inside the ZIP and converted to a string
-   * with UTF-8 encoding.
-   *
-   * @param zipFile The ZIP file from which records will be retrieved.
-   * @return A list of strings, where each string represents the contents of a file from the ZIP.
-   * @throws IOException If an issue occurs while accessing or reading the ZIP file.
-   */
-  public List<String> getRecordsFromZipFile(ZipFile zipFile) throws IOException {
-    final List<InputStream> streams = getContentFromZipFile(zipFile);
-    final List<String> result = new ArrayList<>(streams.size());
-    for (InputStream stream : streams) {
-      result.add(IOUtils.toString(stream, StandardCharsets.UTF_8));
-    }
-    return result;
-  }
-
-  private List<InputStream> getContentFromZipFile(ZipFile zipFile) throws IOException {
-    final List<InputStream> result = new ArrayList<>();
-    final Iterator<? extends ZipEntry> entries = zipFile.stream().iterator();
-    while (entries.hasNext()) {
-      final ZipEntry zipEntry = entries.next();
-      if (zipEntry.isDirectory() || skipMacFiles(zipEntry)) {
-        continue;
-      }
-      result.add(zipFile.getInputStream(zipEntry));
-    }
-    return result;
-  }
-
-  private boolean skipMacFiles(ZipEntry zipEntry) {
-    return zipEntry.getName().startsWith(MAC_TEMP_FOLDER) || zipEntry.getName().endsWith(MAC_TEMP_FILE);
   }
 }
